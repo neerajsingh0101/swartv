744836623d102d4b4bd58b0bc0a6538813e9c477
Neeraj Singh <neerajdotname@gmail.com>
2011-05-22 22:15:06 -0400

    fix js

--- config/initializers/videos.rb
+++ config/initializers/videos.rb
@@ -1,1 +1,10 @@
-YOUTUBE_IDS = ["QZYS8xoBYhM", "kBfdQYpF1NA", "Qy_bQSKysYw", "L_zRDchkDXg", "qla13aWrNP4", "RckfbOm84Qc"]
+#YOUTUBE_IDS = ["QZYS8xoBYhM", "kBfdQYpF1NA", "Qy_bQSKysYw", "L_zRDchkDXg", "qla13aWrNP4", "RckfbOm84Qc"]
+YOUTUBE_IDS = %w(
+QZYS8xoBYhM
+kBfdQYpF1NA
+Qy_bQSKysYw
+L_zRDchkDXg
+qla13aWrNP4
+RckfbOm84Qc
+)
+
